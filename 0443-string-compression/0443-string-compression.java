76b886ba460d0d610e61b595aed93d422a048388
LUCKY WALIA <luckywalia2004@gmail.com>
2026-07-02 12:57:56 +0530

    Time: 1 ms (99.87%), Space: 45.3 MB (80.22%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,27 +1,27 @@
 class Solution {
     public int compress(char[] chars) {
-        int n=chars.length;
-        int idx=0;
+        
+int idx=0;
+int n=chars.length;
 
-        for(int i=0;i<n;i++){
+for(int i=0;i<n;i++){
 char ch=chars[i];
 int c=0;
-
 while(i<n && chars[i]==ch){
-    c++;
-    i++;
+c++;
+i++;
 }
 chars[idx++]=ch;
-
 if(c>1){
-    String cstr=Integer.toString(c);
-    for(char dig:cstr.toCharArray()){
-        chars[idx++]=dig;
-    }
+String nstr=Integer.toString(c);
+for(char dig:nstr.toCharArray()){
+chars[idx++]=dig;
+}
 }
 i--;
- 
-        }
+
+}
+
 return idx;
     }
 }
