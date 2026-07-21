9bd0a319b508c819e8c53372c9db075b758122a6
LUCKY WALIA <luckywalia2004@gmail.com>
2026-07-21 18:32:40 +0530

    Time: 16 ms (79.91%), Space: 47 MB (36.9%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int len=s.length();
+
+        for(int i=len/2;i>=1;i--){
+if(len%i==0){
+    String sub=s.substring(0,i);
+    StringBuilder st=new StringBuilder();
+
+    for(int j=1;j<=len/i;j++){
+st.append(sub);
+    }
+
+if(st.toString().equals(s)) return true;
+
+}
+        }
+return false;
+    }
+}
