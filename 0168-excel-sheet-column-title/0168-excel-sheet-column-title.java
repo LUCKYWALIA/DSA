0e0bccf2dddd500bc7389466b9882c60506d6624
LUCKY WALIA <luckywalia2004@gmail.com>
2026-07-23 20:19:30 +0530

    Time: 0 ms (100%), Space: 42.6 MB (40.81%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String convertToTitle(int n) {
+        
+    StringBuilder res= new StringBuilder();
+
+    while(n>0){
+n--;
+res.insert(0,(char)('A'+n%26));
+n/=26;
+    }
+
+return res.toString();
+
+    }
+}
