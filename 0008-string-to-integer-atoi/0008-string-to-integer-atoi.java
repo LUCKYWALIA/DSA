e350ff64f0806e63f22706a624d448871a2fffbc
LUCKY WALIA <luckywalia2004@gmail.com>
2026-08-23 21:49:15 +0530

    Time: 1 ms (100%), Space: 43.8 MB (58.73%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,33 +1,22 @@
 class Solution {
+    int getNum(String s,int i,long res,int sign){
+        if(i>=s.length() || !Character.isDigit(s.charAt(i))) return sign* (int)res;
+        res=res*10+(s.charAt(i)-'0');
+        if(sign*res>=Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if(sign*res<=Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+             return getNum(s,i+1,res,sign);
+    }
     public int myAtoi(String s) {
         s=s.trim();
-        int sign=1;
+        if(s==null || s.isEmpty()) return 0;
         int i=0;
-        long res=0;
-
-if(s.length()==0) return 0;
-
-if(s.charAt(0)=='-'){
-    sign=-1;
-    i++;
-}
-else if(s.charAt(0)=='+') i++;
-             
-
-while(i<s.length()){
-    char ch=s.charAt(i);
-
-if(ch<'0' || ch>'9') break;
-
-res=res*10+(ch-'0');
-
-if(sign*res>Integer.MAX_VALUE) return Integer.MAX_VALUE;
-if(sign*res<Integer.MIN_VALUE) return Integer.MIN_VALUE;
-
-i++;
-}
-
-
-return (int) (sign*res);
+        int sign=1;
+        if(s.charAt(0)=='-' || s.charAt(0)=='+' ){
+sign=s.charAt(0)=='-' ?-1:1;
+            i++;
+        }
+        while(i<s.length() && s.charAt(i)=='0') i++;
+        return getNum(s,i,0L,sign);
     }
 }
