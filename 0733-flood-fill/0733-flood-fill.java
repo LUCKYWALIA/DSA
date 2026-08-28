b8e31c012ec4456c3dccd277acfa9935fa39558b
LUCKY WALIA <luckywalia2004@gmail.com>
2026-08-28 23:12:53 +0530

    Time: 0 ms (100%), Space: 46.7 MB (79.26%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        
+if(image[sr][sc]==color) return image;
+
+fill(image,sr,sc,color,image[sr][sc]);
+
+return image;
+
+    }
+
+    void fill(int [][] image,int sr,int sc,int color,int curr){
+if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length) return;
+if(curr!=image[sr][sc]) return;
+image[sr][sc]=color;
+fill(image,sr-1,sc,color,curr);
+fill(image,sr+1,sc,color,curr);
+fill(image,sr,sc-1,color,curr);
+fill(image,sr,sc+1,color,curr);
+
+
+    }
+}
